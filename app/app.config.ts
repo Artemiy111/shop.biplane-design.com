697674df4f99e34334cd221e42650d65069dd5d8
Artemiy111 <mrart111@mail.ru>
2025-04-24 18:28:13 +0300

    fix: app.config.ts

---
+++ app/app.config.ts
@@ -0,0 +1,15 @@
+export default defineAppConfig({
+  ui: {
+    colors: {
+      primary: 'amber',
+    },
+    button: {
+      slots: {
+        base: 'font-bold cursor-pointer',
+      },
+    },
+  },
+  icon: {
+    mode: 'svg',
+  },
+})
