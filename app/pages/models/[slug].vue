9580f2875496420922b1d38ec053445878a04387
Artemiy111 <mrart111@mail.ru>
2025-04-24 20:17:40 +0300

    chore: add model/set pages templates

---
+++ app/pages/models/[slug].vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ProductPage } from '~/src/pages/product'
+
+const route = useRoute('models-slug')
+const slug = computed(() => route.params.slug)
+</script>
+
+<template>
+  <ProductPage
+    :is-model="true"
+    :is-set="false"
+    :slug="slug"
+  />
+</template>
---
+++ app/src/pages/product/ProductPage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { PageHeading } from '~/src/shared/ui/blocks/page-heading'
+
+const { isModel, isSet, slug } = defineProps<{
+  isModel: boolean
+  isSet: boolean
+  slug: string
+}>()
+</script>
+
+<template>
+  <main class="container flex flex-col">
+    <PageHeading>{{ slug }}</PageHeading>
+  </main>
+</template>
